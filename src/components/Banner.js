98ca838c3a5c0efcaccc017fec7b01adbed349cc
Leonel <abranteleo@gmail.com>
2026-09-24 17:45:21 -0400

    updated (node)

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <span className="tagline">Welcome to my Portfolio</span>
+                  <span className="tagline">Welcome</span>
                   <h1>{`Hi I'm Leonel Abrante, `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>I’m a frontend developer I'll show my works and my knowledges
                     Also you will be able to contact me through my social media
